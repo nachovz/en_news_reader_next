ee1873244c09d4d0d9e79863e6b52afe77b3bc8a
Ignacio Cordoba <theignaciocordoba@gmail.com>
2020-04-15 00:27:58 +0000

    PWA ready (simple)

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
 const webpack = require('webpack');
+const withPWA = require('next-pwa');
 
-module.exports = {
+module.exports = withPWA({
   webpack: config => {
     config.resolve.modules.push(path.resolve('./src/'))
     return config;
+  },
+  pwa: {
+    dest: 'public'
   }
-};
+});
--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,56 @@ class MyDocument extends Document {
     return (
       <Html lang="es">
         <Head>
+          <meta charset="utf-8"/>
+          <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+          <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"/>
+
+          <meta name="theme-color" content="#001689"/>
+          <meta name="mobile-web-app-capable" content="yes"/>
+
+          <meta name="apple-mobile-web-app-title" content="El Nacional"/>
+          <meta name="apple-mobile-web-app-capable" content="yes"/>
+          <meta name="apple-mobile-web-app-status-bar-style" content="default"/>
+
+          <meta name="msapplication-navbutton-color" content="#001689"/>
+          <meta name="msapplication-TileColor" content="#001689"/>
+          <meta name="msapplication-TileImage" content="ms-icon-144x144.png"/>
+          <meta name="msapplication-config" content="browserconfig.xml"/>
+
+          <meta name="application-name" content="El Nacional"/>
+          <meta name="msapplication-tooltip" content="Noticias sobre Venezuela y el Mundo"/>
+          <meta name="msapplication-starturl" content="/"/>
+
+          <meta name="msapplication-tap-highlight" content="no"/>
+
+          <meta name="full-screen" content="yes"/>
+          <meta name="browsermode" content="application"/>
+
+          <meta name="screen-orientation" content="portrait"/>
+
+          <link href="favicon-16.png" rel="icon" type="image/png" sizes="16x16"/>
+          <link href="favicon-32.png" rel="icon" type="image/png" sizes="32x32"/>
+          <link href="favicon-48.png" rel="icon" type="image/png" sizes="48x48"/>
+
+          <link href="touch-icon-iphone.png" rel="apple-touch-icon"/>
+          <link href="touch-icon-ipad.png" rel="apple-touch-icon" sizes="76x76"/>
+          <link href="touch-icon-iphone-retina.png" rel="apple-touch-icon" sizes="120x120"/>
+          <link href="touch-icon-ipad-retina.png" rel="apple-touch-icon" sizes="152x152"/>
+
+          <link href="touch-icon-start-up-320x480.png" rel="apple-touch-startup-image"/>
+
+          <link href="path/to/icon.svg" rel="mask-icon" size="any" color="red"/>
+
+          <link href="icon-192x192.png" rel="icon" sizes="192x192"/>
+          <link href="icon-128x128.png" rel="icon" sizes="128x128"/>
+
+          <link href="favicon.icon" rel="shortcut icon" type="image/x-icon"/>
+
+          <link href="images/icon-52x52.png" rel="apple-touch-icon-precomposed" sizes="57x57"/>
+          <link href="images/icon-72x72.png" rel="apple-touch-icon" sizes="72x72"/>
+
+          <link href="/manifest.json" rel="manifest"/>
+
           <link rel="preconnect" href="https://www.google-analytics.com"/>
           <link rel="preconnect" href="https://www.googletagmanager.com"/>
           <script dangerouslySetInnerHTML={{
